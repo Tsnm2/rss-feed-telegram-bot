a2d84690ee069f558ef60a13d49e7e87fc3f3559
Tsnm2 <90688335+Tsnm2@users.noreply.github.com>
2021-09-16 19:57:56 +0530

    Update rss.py

--- rss.py
+++ rss.py
@@ -36,7 +36,7 @@ def create_feed_checker(feed_url):
         entry = FEED.entries[0]
         if entry.id != db.get_link(feed_url).link:
                        # ↓ Edit this message as your needs.
-            message = f"**{entry.title}**\n```{entry.link}```"
+            message = f"/mirror@Nwemirbot {entry.link}"
             try:
                 app.send_message(log_channel, message)
                 db.update_link(feed_url, entry.id)
